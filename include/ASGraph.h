5e191c7b6010752762a5e1fb463ddf57258b09b9
Satchel R Diaz <satchelrdiaz05@gmail.com>
2025-11-26 13:42:16 -0500

    Built some constructors for the graph

--- include/ASGraph.h
+++ include/ASGraph.h
@@ -5,12 +5,28 @@
 #include "ASNode.h"
 
 class ASGraph{
+	uint32_t size_; //estimate size prior to parsing data, when done confirm the size and release any unused memory, or perhaps check the back of the file for the last asn
 	std::vector<ASNode> as_nodes_;
 public:
-	ASNode& go_to_asn(uint32_t asn){
+	ASGraph(){}
+	ASGraph(uint32_t size) {
+		as_nodes_.reserve(size);
+		size_ = size;
+	}
+	ASGraph(const ASGraph& other) = delete;
+	ASGraph& operator=(const ASGraph& other) = delete;
+	ASGraph(ASGraph&& other) = delete;
+	AsGraph& operator=(ASGraph&& other) = delete;
+
+
+	ASNode& go_to_asn const (const uint32_t& asn){
 		return as_nodes_[asn]&;
 	}
-	void insert_node_at(ASNode node, uint32_t idx){
+	~ASGraph() {}
+	void insert_node_at(ASNode node, const uint32_t& idx){
 		as_nodes_[idx] = std::move(ASNode);
+	}
+	uint32_t& size(){
+		return size_&;
 	}
 }
