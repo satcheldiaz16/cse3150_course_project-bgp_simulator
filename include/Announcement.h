e818d9637545665151dd99b3bc219ef0b562749c
Satchel R Diaz <satchelrdiaz05@gmail.com>
2025-12-01 16:33:32 -0500

    The plan has been set in motion (readme)

--- include/Announcement.h
+++ include/Announcement.h
@@ -9,10 +9,12 @@ public:
 	uint32_t prefix_id; //can store our prefixes in a hashmap elsewhere
 	//24 bytes
 	std::string prefix; // 1.2.0.0/16 or some IPv6 prefix
-	uint32_t start_index;
-	uint32_t end_index;
+	//uint32_t start_index;
+	//uint32_t end_index;
+    ASNode* host;
+    Announcement* prev = nullptr; //nullptr denotes origin of path 
 	//24 bytes
-	std::vector<uint32_t> as_path;
+	//std::vector<uint32_t> as_path;
 	//1 byte
 	short relationship; // 0 for customers, 1 for peers, 2 for providers, 3 for origin
 	//4 bytes
